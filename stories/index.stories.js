b978d267b3d7a5829f2275ab7dfc515ddfc548f1
taigen hamada <taigen.hamada@siliconstudio.co.jp>
2018-10-02 16:26:03 +0900

    update stories

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -2,18 +2,36 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+import centered from '@storybook/addon-centered'
 
-import { Button, Welcome } from '@storybook/react/demo';
+import Card from '../src/components/Card'
+import Header from '../src/components/Header'
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+storiesOf('Card', module)
+  .addDecorator(centered)
+  .add('checked: true', () => (
+    <Card
+      image={'https://i.pinimg.com/originals/0c/da/23/0cda2360f6f0200cd2547e7ad77a7eb5.jpg'}
+      title={'Dummy'}
+      checked={true}
+      onEyeButtonClick={action('Eye Button clicked')}
+      onDownloadButtonClick={action('download clicked')}
+      onCheckboxClick={action('checkbox clicked')}
+    />)
+  )
+  .add('checked: false', () => (
+    <Card
+      image={'https://i.pinimg.com/originals/0c/da/23/0cda2360f6f0200cd2547e7ad77a7eb5.jpg'}
+      title={'Dummy'}
+      checked={false}
+      onEyeButtonClick={action('Eye Button clicked')}
+      onDownloadButtonClick={action('download clicked')}
+      onCheckboxClick={action('checkbox clicked')}
+    />)
+  )
 
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>
-  ));
+
+storiesOf('Header', module)
+  .add('default', () => (
+    <Header/>
+  ))
