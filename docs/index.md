4ec45f37c8a05b0b4098df13e962243892407fb4
hamada <t.hamada@fair-next-innovation.co.jp>
2019-12-12 11:29:54 +0900

    update: 微修正

--- docs/index.md
+++ docs/index.md
@@ -49,6 +49,10 @@ like Hover Zoom, even if you can't mass download images with it.
 
 ## Change log
 
+### Version 2.3.1
+
+* Update some modules
+
 ### Version 2.3.0
 
 * Show progress bar
--- public/scripts/background.js
+++ public/scripts/background.js
@@ -1,3 +1,6 @@
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.tabs.create({ url: "https://hatai.github.io/image-downloader/" });
-});
+chrome.runtime.onInstalled.addListener(function (details) {
+  const reason = details.reason
+  if (reason === 'install' || reason === 'update') {
+    chrome.tabs.create({url: 'https://hatai.github.io/image-downloader/'})
+  }
+})
